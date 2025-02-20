51777a8c8658c99474bb837d8fadd042137ff886
hana/mori <sagiri.mori@gmail.com>
2025-02-20 20:50:28 +0900

    ver2.0

---
+++ ver2.0/src/fdf.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hana/hmori <sagiri.mori@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 15:19:43 by hana/hmori        #+#    #+#             */
+/*   Updated: 2025/02/20 20:05:38 by hana/hmori       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fdf.h"
+// #include "fdf.h"
+
+static int	**assig_arr(int fd, int length, int width_size)
+{
+	char	*ret;
+	int		*sp_arr;
+	int		**result;
+
+	result = NULL;
+	ret = get_next_line(fd);
+	if (ret == NULL)
+		return (ft_calloc(sizeof(int *), length + 1));
+	if (ft_strchr(ret, '\n'))
+		ret[ft_strchr(ret, '\n') - ret] = '\0';
+	sp_arr = ft_split_toi(ret, ' ');
+	free(ret);
+	if (sp_arr == NULL)
+		return (NULL);
+	if (*sp_arr == 0)
+		ft_putstr_fd("No data found.\n", STDERR_FILENO);
+	else if (width_size == 0 || *sp_arr == width_size)
+		result = assig_arr(fd, length + 1, *sp_arr);
+	else
+		ft_putstr_fd("Found wrong line length. Exiting.\n", STDERR_FILENO);
+	if (result)
+		result[length] = sp_arr;
+	else
+		free(sp_arr);
+	return (result);
+}
+
+void	mlx(char *name, int **mat_arr)
+{
+	t_vars	vars;
+
+	vars.varr = crdarr_init(mat_arr);
+	// vars.mat_arr = mat_arr;
+	parameter_init(&vars);
+	vars.arrof = arr_off_init(vars.varr.arr);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, name);
+	vars.idata.img = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
+	vars.idata.addr = mlx_get_data_addr(vars.idata.img,
+			&vars.idata.bits_per_pixel,
+			&vars.idata.size_line, &vars.idata.endian);
+	mlx_hook(vars.win, KeyPress, KeyPressMask, key_press, &vars);
+	mlx_hook(vars.win, DestroyNotify, SubstructureNotifyMask, window_destroy,
+		&vars);
+	mlx_hook(vars.win, ButtonPress, ButtonPressMask, mouse_press, &vars);
+	mlx_hook(vars.win, ButtonRelease, ButtonReleaseMask, mouse_release, &vars);
+	mlx_hook(vars.win, MotionNotify, PointerMotionMask, mouse_move, &vars);
+	mlx_loop_hook(vars.mlx, render_frame, &vars);
+	mlx_loop(vars.mlx);
+	mlx_destroy_display(vars.mlx);
+	free(vars.mlx);
+}
+
+static void	double_free(int **array)
+{
+	int	**mem;
+
+	mem = array;
+	while (*mem)
+		free(*mem++);
+	free(array);
+}
+
+static int	err_turn(char *str)
+{
+	ft_putstr_fd(str, STDERR_FILENO);
+	return (1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	fd;
+	int	**array;
+
+	if (argc != 2)
+		return (err_turn("Usage : ./fdf <filename> [ case_size z_size ]\n"));
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("No file ", STDERR_FILENO);
+		ft_putstr_fd(argv[1], STDERR_FILENO);
+		return (err_turn("\n"));
+	}
+	array = assig_arr(fd, 0, 0);
+	close(fd);
+	if (array == NULL)
+		return (1);
+	if (array[0] == NULL)
+	{
+		free(array);
+		return (err_turn("No data found.\n"));
+	}
+	mlx(*argv, array);
+	double_free(array);
+	return (0);
+}
---
+++ ver2.0/src/graphic.c
@@ -0,0 +1,207 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   graphic.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hana/hmori <sagiri.mori@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 11:32:12 by hana/hmori        #+#    #+#             */
+/*   Updated: 2025/02/20 20:05:40 by hana/hmori       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fdf.h"
+// #include "fdf.h"
+
+static void	my_mlx_pixel_put(t_idata *img, int x, int y, int color)
+{
+	char	*dst;
+
+	if (x < 0 || WIDTH <= x || y < 0 || HEIGHT <= y)
+		return ;
+	dst = img->addr + (y * img->size_line + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+// bresenham line
+static void	put_line(t_idata *idata, t_vec3 q1, t_vec3 q2)
+{
+	int		steps;
+	float	dx;
+	float	dy;
+
+	dx = q2.x - q1.x;
+	dy = q2.y - q1.y;
+	steps = fabs(dx);
+	if (steps < fabs(dy))
+		steps = fabs(dy);
+	dx = dx / steps;
+	dy = dy / steps;
+	while (steps--)
+	{
+		my_mlx_pixel_put(idata, roundf(q1.x), roundf(q1.y), 0xFFFFFFFF);
+		q1.x += dx;
+		q1.y += dy;
+	}
+}
+
+static int	render_len(int my, t_quater *pr_len, t_idata *newimg, t_vars *vars)
+{
+	int			mx;
+	t_quater	q;
+	t_quater	pr_crd;
+
+	if (pr_len == NULL)
+		return (1);
+	mx = -1;
+	while (++mx < vars->varr.width)
+	{
+		q = quater_rotate(vars->rqv,
+				arr_off_set(mx, my, vars->varr.arr[my][mx + 1], vars->arrof));
+		if (mx)
+			put_line(newimg, crd_off_set(pr_crd.v, vars->param),
+				crd_off_set(q.v, vars->param));
+		if (my)
+			put_line(newimg, crd_off_set(pr_len[mx].v, vars->param),
+				crd_off_set(q.v, vars->param));
+		pr_crd = q;
+		pr_len[mx] = q;
+	}
+	if (my + 1 < vars->varr.length)
+		render_len(my + 1, pr_len, newimg, vars);
+	else
+		free(pr_len);
+	return (0);
+}
+
+		// t_vec3 test = crd_off_set(q.v, vars->param);
+		// my_mlx_pixel_put(newimg, test.x, test.y, 0xFFFFFFFF);
+
+int	render_frame(t_vars *vars)
+{
+	t_idata	newimg;
+
+	if (vars->gflag.fl & (1 << RE_GRAPHIC))
+	{
+		vars->gflag.fl &= ~(1 << RE_GRAPHIC);
+		newimg.img = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
+		newimg.addr = mlx_get_data_addr(newimg.img, &newimg.bits_per_pixel,
+				&newimg.size_line, &newimg.endian);
+		render_len(0, malloc(sizeof(t_quater) * vars->varr.width),
+			&newimg, vars);
+		mlx_put_image_to_window(vars->mlx, vars->win, newimg.img, 0, 0);
+		mlx_destroy_image(vars->mlx, vars->idata.img);
+		vars->idata = newimg;
+	}
+	return (0);
+}
+
+void	parameter_init(t_vars *vars)
+{
+	vars->rqv = quater_normalize(quater_axis_angle
+			(DEF_VIEW_X, DEF_VIEW_Y, DEF_VIEW_Z, DEF_VIEW_AXIS * M_PI / 180));
+	vars->qv = vars->rqv;
+	vars->arrof = arr_off_init(vars->varr.arr);
+	vars->param.zoom = DEF_ZOOM;
+	vars->gflag.fl = (1 << RE_GRAPHIC) | (0 << MOUSE_PRESS);
+}
+
+// #include <time.h>
+
+// static int	render_frame(t_vars *vars)
+// {
+// 	t_idata	newimg;
+
+// 	clock_t	start_time;
+// 	clock_t	end_time;
+
+// 	if (vars->gflag.refresh == GLAPH_ON)
+// 	{
+// 		vars->gflag.refresh = GLAPH_OFF;
+// 		newimg.img = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
+// 		newimg.addr = mlx_get_data_addr(newimg.img, &newimg.bits_per_pixel,
+// 				&newimg.size_line, &newimg.endian);
+
+// 		start_time = clock();
+// 		render_len(0, malloc(sizeof(t_quater) * vars->varr.width), &newimg, vars);
+// 		end_time = clock();
+// 		printf("Time taken for malloc: %f seconds\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+// 		mlx_put_image_to_window(vars->mlx, vars->win, newimg.img, 0, 0);
+// 		mlx_destroy_image(vars->mlx, vars->idata.img);
+// 		vars->idata = newimg;
+// 	}
+// 	return (0);
+// }
+
+
+
+// static t_coord	set_coord(int x, int y, int z)
+// {
+// 	t_coord	crd;
+
+// 	crd.x = x;
+// 	crd.y = y;
+// 	crd.z = z;
+// 	return (crd);
+// }
+
+// static t_quater	render_len(int mx, int my, t_idata *newimg, t_vars *vars)
+// {
+// 	t_quater	q;
+// 	t_quater	next;
+
+// 	q = quater_rotate(vars->qv,
+// 			quater_axis_angle(mx - vars->set.x, my - vars->set.y,
+// 				(vars->set.z - vars->mat_arr[my][mx + 1]) * 0.1, M_PI));
+// 	if (mx + 1 < vars->mat_arr[my][0])
+// 	{
+// 		next = render_len(mx + 1, my, newimg, vars);
+// 		put_line(newimg, set_coord(q.x * vars->set.zoom + WIDTH / 2,
+// 				q.y * vars->set.zoom + HEIGHT / 2, q.z),
+// 			set_coord(next.x * vars->set.zoom + WIDTH / 2,
+// 				next.y * vars->set.zoom + HEIGHT / 2, next.z));
+// 	}
+// 	if (vars->mat_arr[my + 1])
+// 	{
+// 		next = quater_rotate(vars->qv, quater_axis_angle(mx - vars->set.x,
+// 					my + 1 - vars->set.y,
+// 					(vars->set.z - vars->mat_arr[my + 1][mx + 1]) * 0.1, M_PI));
+// 		put_line(newimg, set_coord(q.x * vars->set.zoom + WIDTH / 2,
+// 				q.y * vars->set.zoom + HEIGHT / 2, q.z),
+// 			set_coord(next.x * vars->set.zoom + WIDTH / 2,
+// 				next.y * vars->set.zoom + HEIGHT / 2, next.z));
+// 	}
+// 	return (q);
+// }
+
+// #include <time.h>
+
+// static int	render_frame(t_vars *vars)
+// {
+// 	int			len;
+// 	t_idata		newimg;
+
+// 	clock_t	start_time;
+// 	clock_t	end_time;
+
+// 	if (vars->gflag.refresh == GLAPH_ON)
+// 	{
+// 		vars->gflag.refresh = GLAPH_OFF;
+// 		newimg.img = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
+// 		newimg.addr = mlx_get_data_addr(newimg.img, &newimg.bits_per_pixel,
+// 				&newimg.size_line, &newimg.endian);
+
+// 		start_time = clock();
+// 		len = 0;
+// 		while (vars->mat_arr[len])
+// 			render_len(0, len++, &newimg, vars);
+// 		end_time = clock();
+// 		printf("Time taken for malloc: %f seconds\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+// 		mlx_put_image_to_window(vars->mlx, vars->win, newimg.img, 0, 0);
+// 		mlx_destroy_image(vars->mlx, vars->idata.img);
+// 		vars->idata = newimg;
+// 	}
+// 	return (0);
+// }
